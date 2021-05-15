a1506132694177cee7289df0e48eb71de1bb56ce
CeeTee <clifftangel@gmail.com>
2021-05-15 11:54:56 +0700

    Signup function

--- app/src/main/java/com/example/torem/front/SignUpActivity.kt
+++ app/src/main/java/com/example/torem/front/SignUpActivity.kt
@@ -1,12 +1,98 @@
 package com.example.torem.front
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.example.torem.BaseActivity
 import com.example.torem.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.example.torem.databinding.ActivitySignUpBinding
+import com.example.torem.home.HomeActivity
+import com.google.android.material.snackbar.Snackbar
 
-class SignUpActivity : AppCompatActivity() {
+
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        val binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupButton.setOnClickListener{
+            validateRegisterDetails(binding)
+            val isValid: Boolean = validateRegisterDetails(binding)
+            when{
+                isValid == true->{
+                    val email: String = binding.inputEmail.text.toString().trim{it<= ' '}
+                    val password: String = binding.inputPassword.text.toString().trim{it<= ' '}
+
+                    //Create Firebase instance and register user
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult>{ task ->
+                                //if registration is successful
+                                if(task.isSuccessful){
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                    Toast.makeText(
+                                        this,
+                                        "You are registered!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent = Intent(this, HomeActivity::class.java)
+                                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra("user_id",firebaseUser.uid)
+                                    intent.putExtra("email", email)
+                                    startActivity(intent)
+                                    finish()
+                                }else{
+                                    Toast.makeText(
+                                        this,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+                }
+            }
+        }
     }
+    
+    private fun validateRegisterDetails(binding: ActivitySignUpBinding):Boolean{
+        return when{
+            TextUtils.isEmpty(binding.inputEmail.text.toString().trim{it<=' '})->{
+                showErrorSnackBar(resources.getString(R.string.no_email), true)
+                false
+            }
+            TextUtils.isEmpty(binding.inputName.text.toString().trim{it<=' '})->{
+                showErrorSnackBar(resources.getString(R.string.no_name), true)
+                false
+            }
+            TextUtils.isEmpty(binding.inputPassword.text.toString().trim{it<=' '})->{
+                showErrorSnackBar(resources.getString(R.string.no_password), true)
+                false
+            }
+            TextUtils.isEmpty(binding.inputVerifyPassword.text.toString().trim{it<=' '})->{
+                showErrorSnackBar(resources.getString(R.string.no_conf_password), true)
+                false
+            }
+            binding.inputPassword.text.toString().trim{it<=' '}!= binding.inputVerifyPassword.text.toString().trim{it<=' '}->{
+                showErrorSnackBar(resources.getString(R.string.password_no_match), true)
+                false
+            }
+            else->{
+                showErrorSnackBar("You're in!", false)
+                true
+            }
+        }
+    }
+
+
 }
