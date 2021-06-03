41e4f1229617204dea39fd324d3a13b04c4f855e
CeeTee <clifftangel@gmail.com>
2021-06-03 16:13:11 +0700

    Get started

--- app/src/main/java/com/example/torem/Activity/SpalshScreenActivity.kt
+++ app/src/main/java/com/example/torem/Activity/SpalshScreenActivity.kt
@@ -1,12 +1,16 @@
 package com.example.torem.Activity
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.torem.R
+import com.example.torem.front.GetStarted
 import com.example.torem.front.LoginActivity
+import com.example.torem.util.Utils
 import com.google.firebase.auth.FirebaseAuth
 
 class SpalshScreenActivity : AppCompatActivity() {
@@ -17,16 +21,37 @@ class SpalshScreenActivity : AppCompatActivity() {
 
         var user = FirebaseAuth.getInstance().currentUser
 
+        val sharedPreferences = getSharedPreferences(
+                "getstarted",
+                Context.MODE_PRIVATE
+        )
+
+        var isJustInstalled = sharedPreferences.getBoolean("justInstalled", true)
+
+
         if(user!= null){
             Handler(Looper.getMainLooper()).postDelayed( {
                 startActivity(Intent (this, HomeActivity::class.java))
                 finish()
             },5000)
         }else{
-            Handler(Looper.getMainLooper()).postDelayed( {
-                startActivity(Intent (this, LoginActivity::class.java))
-                finish()
-            },5000)
+            if(isJustInstalled) {
+                val editor: SharedPreferences.Editor = sharedPreferences.edit()
+
+                editor.putBoolean("justInstalled", false)
+
+                editor.apply()
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this, GetStarted::class.java))
+                    finish()
+                }, 5000)
+            }else {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }, 5000)
+            }
         }
 
 
--- app/src/main/java/com/example/torem/front/GetStarted.kt
+++ app/src/main/java/com/example/torem/front/GetStarted.kt
@@ -1,12 +1,24 @@
 package com.example.torem.front
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.torem.R
+import com.example.torem.databinding.ActivityGetStartedBinding
 
 class GetStarted : AppCompatActivity() {
+    private lateinit var binding: ActivityGetStartedBinding
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        binding = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.getstarted.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
     }
 }
